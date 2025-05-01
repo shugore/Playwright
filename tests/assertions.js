0b2b2ba8c06fbdeb0e40e8bce5b84b4cced2474d
SHUBHAM  GORE <shubhamgore001@gmail.com>
2025-05-01 23:48:50 +0530

    added codegen script

---
+++ tests/assertions.js
@@ -0,0 +1,4 @@
+await expect(locator).toBeAttached()	//Element is attached
+await expect(locator).toBeChecked()	    //Checkbox is checked
+await expect(locator).toBeDisabled()	//Element is disabled
+await expect(locator).toBeEditable()	//Element is editable
---
+++ tests/codegenScript.js
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+//Command to generate it
+
+// npx platwright codegen
+test('test', async ({ page }) => {
+  await page.goto('https://www.demoblaze.com/');
+  await page.getByRole('link', { name: 'Log in' }).click();
+  await page.locator('#loginusername').click();
+  await page.locator('#loginpassword').click();
+  page.once('dialog', dialog => {
+    console.log(`Dialog message: ${dialog.message()}`);
+    dialog.dismiss().catch(() => {});
+  });
+  await page.getByRole('button', { name: 'Log in' }).click();
+});
