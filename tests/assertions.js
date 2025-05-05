426881eac3067ad0df8649b804f57905a63ff72b
SHUBHAM  GORE <shubhamgore001@gmail.com>
2025-05-05 23:31:37 +0530

    soft assert

--- tests/assertions.js
+++ tests/assertions.js
@@ -2,3 +2,10 @@ await expect(locator).toBeAttached()	//Element is attached
 await expect(locator).toBeChecked()	    //Checkbox is checked
 await expect(locator).toBeDisabled()	//Element is disabled
 await expect(locator).toBeEditable()	//Element is editable
+
+//soft assertions
+
+await expect.soft(locator).toBeAttached()
+await expect.soft(locator).toBeEditable()
+await expect.soft(locator).toBeChecked()	    //Checkbox is checked
+await expect.soft(locator).toBeDisabled()
