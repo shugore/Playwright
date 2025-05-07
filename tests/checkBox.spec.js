516c041b22540b9c850defc9fb2b7d0f1262188c
SHUBHAM  GORE <shubhamgore001@gmail.com>
2025-05-08 00:22:03 +0530

    check uncheck boxes

--- tests/checkBox.spec.js
+++ tests/checkBox.spec.js
@@ -9,9 +9,27 @@ test('has title', async ({ page }) => {
 
   await expect(await page.locator('xyz')).toBeChecked()
 
+  //isChecked will verify for it and compare with true using toBeWorthy
   await expect(await page.locator('xyz').isChecked).toBeTruthy()
 
 
+  //isChecked will verify for it and compare with false using toBeFalsy
+  await expect(await page.locator('xyz').isChecked).toBeFalsy()
+
+  const checkBoxLocators =["Locator1","locator2","locator3"]
+
+  for(const locator of checkBoxLocators){
+
+    await page.locator('locator').check()
+  }
+
+  for(const locator of checkBoxLocators){
+
+   if( await page.locator('locator').isChecked()){
+
+    await page.locator('locator').uncheck()
+   }
+  }
 
   await page.waitForTimeout(5000);
 });
