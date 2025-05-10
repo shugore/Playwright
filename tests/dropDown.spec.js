daa693804562f1d54150a41b9a230af59584300d
SHUBHAM  GORE <shubhamgore001@gmail.com>
2025-05-11 00:24:17 +0530

    assertion and multi select dropdown

--- tests/dropDown.spec.js
+++ tests/dropDown.spec.js
@@ -1,7 +1,7 @@
 // @ts-check
 import { test, expect } from "@playwright/test";
 
-test("has title", async ({ page }) => {
+test("Select single Value from dropdown", async ({ page }) => {
   // await page.goto('https://testautomationpractice.blogspot.com/');
 
   //select dropdwn using label
@@ -19,6 +19,59 @@ test("has title", async ({ page }) => {
   //direct using selectoption
   await page.selectOption("#country", "India"); //its tetx only
 
+  //Assertions for dropdown
 
+  //1> Check number of optuons in dropdown
+  const options = await page.locator("#country options");
+  await expect(options).toHaveCount(10);
+
+  //2> Check number of optuons in dropdown
+  const option = await page.$$("#country option");
+  //console.log("Number of Options:",option.length)
+  await expect(option.length).toBe(10);
+
+  //3> Check presence of value in dropdown
+
+  const content = await page.locator("#country").textContent();
+  await expect(content?.includes("India")).toBeTruthy();
+
+  //4> Check presence of value in dropdown using looping
+  const optionss = await page.$$("#country option");
+  let status = false;
+
+  for (const option of optionss) {
+    //console.log(await option.textContent());
+
+    let value = await option.textContent();
+
+    if (value?.includes("France")) {
+      status = true;
+      break;
+    }
+  }
+  await expect(status).toBeTruthy();
   await page.waitForTimeout(5000);
+});
+
+test("Select multiple Value from dropdown", async ({ page }) => {
+  // await page.goto('https://testautomationpractice.blogspot.com/');
+
+  //select dropdwn using label
+  await page.selectOption("#colour", ["Blue", "Red", "White"]); //Assertions for dropdown
+
+  //Assertions
+  //1> Check number of optuons in dropdown
+  const options = await page.locator("#colour options");
+  await expect(options).toHaveCount(5);
+
+  //2> Check number of optuons in dropdown
+  const option = await page.$$("#colour option");
+  //console.log("Number of Options:",option.length)
+  await expect(option.length).toBe(5);
+
+  //3> Check presence of value in dropdown
+
+  const content = await page.locator("#colour").textContent();
+  await expect(content?.includes("Blue")).toBeTruthy();
+
 });
