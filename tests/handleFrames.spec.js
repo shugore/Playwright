37c9c02139eb0f3bc99bb66b96fed72e5f04020a
SHUBHAM  GORE <shubhamgore001@gmail.com>
2025-05-14 00:17:12 +0530

    added frames

---
+++ tests/handleFrames.spec.js
@@ -0,0 +1,26 @@
+// @ts-check
+import { test, expect } from "@playwright/test";
+
+test("Handle Frames", async ({ page }) => {
+  await page.goto("https://ui.vision/demo/webtest/frames/");
+
+  //to find total frames
+
+  const allFrames = await page.frames();
+
+  console.log("Number of frames:-" + allFrames.length);
+
+  //handle frame using url and name
+
+  await page.frame({ url: "url of src" });
+  await page.frame("name");
+
+  //using frame locator
+
+  await page
+    .frameLocator("loctor of frame")
+    .locator("locator of element in frame");
+
+  await page.waitForTimeout(5000);
+});
+
